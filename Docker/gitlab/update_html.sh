f3d475597947327cba6da741e2bb45a46c0f5845
ralf <ralf.zeidler@fridata.de>
2017-04-06 17:26:10 +0200

    html update changed

--- Docker/gitlab/update_html.sh
+++ Docker/gitlab/update_html.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+
+# The following code is needed if a private gitlab is used with ssh key
+
 #cp /var/www/html/git/id_rsa /root/.ssh/id_rsa
 #cp /var/www/html/git/id_rsa.pub /root/.ssh/id_rsa.pub
 #chown 600 /root/.ssh/id_rsa
 #git config --global user.email "publicuser@radio-tracking.eu" && \
 #git config --global user.name "Radio Tracking Eu"
 #eval $(ssh-agent -s) && ssh-add /root/.ssh/id_rsa
+
+# Running on live branch with githab repository
 cd /home/pi/gitrep/raspiv2/
-echo 'Download new repositry: <br>'
+echo 'Download new repositry:'
 git pull
-echo 'Copy new repositry. <br>'
+echo '<br> Copy new repositry.'
 cp -R /home/pi/gitrep/raspiv2/html/ /var/www/
 echo 'Refresh porperty rights. <br>'
-chown -R www-data:www-data /var/www
+chown -R www-data:www-data /var/www/
