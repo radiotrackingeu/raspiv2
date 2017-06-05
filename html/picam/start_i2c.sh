77a19893e8e8f365295aeee0198f1881b1218fc6
ralf <ralf.zeidler@fridata.de>
2017-06-05 21:26:41 +0200

    line starts with ^

--- html/picam/start_i2c.sh
+++ html/picam/start_i2c.sh
@@ -9,28 +9,28 @@
 OLDSTRING="#dtparam=i2c1=on"
 NEWSTRING="dtparam=i2c1=on"
 FILE="/tmp1/config.txt"
-grep -q $OLDSTRING $FILE && 
-    sed -i "s/$OLDSTRING/$NEWSTRING/g" $FILE || echo "$NEWSTRING" >> $FILE
+grep -q $NEWSTRING $FILE && 
+    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo "$NEWSTRING" >> $FILE
 
 OLDSTRING="#dtparam=i2c_arm=on"
 NEWSTRING="dtparam=i2c_arm=on"
 FILE="/tmp1/config.txt"
-grep -q $OLDSTRING $FILE && 
-    sed -i "s/$OLDSTRING/$NEWSTRING/g" $FILE || echo "$NEWSTRING" >> $FILE
+grep -q $NEWSTRING $FILE && 
+    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo "$NEWSTRING" >> $FILE
 
 #Add the modules to /etc/modules
 
 OLDSTRING="#i2c-bcm2708"
 NEWSTRING="i2c-bcm2708"
 FILE="/tmp2/modules"
-grep -q $OLDSTRING $FILE && 
-    sed -i "s/$OLDSTRING/$NEWSTRING/g" $FILE || echo "$NEWSTRING" >> $FILE
+grep -q $NEWSTRING $FILE && 
+    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo "$NEWSTRING" >> $FILE
 
 OLDSTRING="#i2c-dev"
 NEWSTRING="i2c-dev"
 FILE="/tmp2/modules"
-grep -q $OLDSTRING $FILE && 
-    sed -i "s/$OLDSTRING/$NEWSTRING/g" $FILE || echo "$NEWSTRING" >> $FILE
+grep -q $NEWSTRING $FILE && 
+    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo "$NEWSTRING" >> $FILE
 
 # sudo i2cset -y 1 0x70 0x00 0xa5 sudo i2cset -y 1 0x70 0x00 0xa5
 
