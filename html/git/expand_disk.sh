3989487ad9c2ca32e305b21557be55ffd904c232
ralf <ralf.zeidler@fridata.de>
2017-08-01 22:40:13 +0200

    sed with |

--- html/git/expand_disk.sh
+++ html/git/expand_disk.sh
@@ -2,14 +2,14 @@
 
 #expand systems on reboot
 
-OLDSTRING='#@reboot root \/usr\/bin\/raspi-config --expand-rootfs'
-NEWSTRING='@reboot root \/usr\/bin\/raspi-config --expand-rootfs'
+OLDSTRING='#@reboot root /usr/bin/raspi-config --expand-rootfs'
+NEWSTRING='@reboot root /usr/bin/raspi-config --expand-rootfs'
 FILE="/tmp/crontab"
 grep -q "$NEWSTRING" $FILE && 
-    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
+    sed -i "s|^$OLDSTRING|$NEWSTRING|g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
 
-OLDSTRING='#@reboot root sh \/var\/www\/html\/git\/stop_expand.sh'
-NEWSTRING='@reboot root sh \/var\/www\/html\/git\/stop_expand.sh'
+OLDSTRING='#@reboot root sh /var/www/html/git/stop_expand.sh'
+NEWSTRING='@reboot root sh /var/www/html/git/stop_expand.sh'
 FILE="/tmp/crontab"
 grep -q "$NEWSTRING" $FILE && 
-    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
+    sed -i "s|^$OLDSTRING|$NEWSTRING|g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
