ab62c9a8ee909e2c599de57d0d27b0f40330d66f
ralf <ralf.zeidler@fridata.de>
2017-08-01 21:40:18 +0200

    one more fie

---
+++ html/git/stop_expand.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#/usr/bin/raspi-config --expand-rootfs
+
+OLDSTRING='@reboot root /usr/bin/raspi-config --expand-rootfs'
+NEWSTRING='#@reboot root /usr/bin/raspi-config --expand-rootfs'
+FILE="/tmp/crontab"
+grep -q "$OLDSTRING" $FILE && 
+    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
+
+OLDSTRING='@reboot root sh /var/www/html/git/stop_expand.sh'
+NEWSTRING='@reboot root /usr/bin/raspi-config --expand-rootfs'
+FILE="/tmp/crontab"
+grep -q "$OLDSTRING" $FILE && 
+    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
