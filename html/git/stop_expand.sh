99c1b52527e86f40d72fc2f887b2329b0551678b
ralf <ralf.zeidler@fridata.de>
2017-08-01 22:14:42 +0200

    stop expand

--- html/git/stop_expand.sh
+++ html/git/stop_expand.sh
@@ -9,7 +9,7 @@ grep -q "$OLDSTRING" $FILE &&
     sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
 
 OLDSTRING='@reboot root sh /var/www/html/git/stop_expand.sh'
-NEWSTRING='@reboot root /usr/bin/raspi-config --expand-rootfs'
+NEWSTRING='#@reboot root sh /var/www/html/git/stop_expand.sh'
 FILE="/tmp/crontab"
 grep -q "$OLDSTRING" $FILE && 
     sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
