7ee6fc5dc58c913a221047fffa7279ca34a0a47b
ralf <ralf.zeidler@fridata.de>
2017-08-01 22:11:54 +0200

    small fixes

--- html/git/expand_disk.sh
+++ html/git/expand_disk.sh
@@ -6,7 +6,7 @@ OLDSTRING='#@reboot root /usr/bin/raspi-config --expand-rootfs'
 NEWSTRING='@reboot root /usr/bin/raspi-config --expand-rootfs'
 FILE="/tmp/crontab"
 grep -q "$NEWSTRING" $FILE && 
-    sed -i "s/$OLDSTRING/$NEWSTRING/g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
+    sed -i "s/^$OLDSTRING/$NEWSTRING/g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
 
 OLDSTRING='#@reboot root sh /var/www/html/git/stop_expand.sh'
 NEWSTRING='@reboot root sh /var/www/html/git/stop_expand.sh'
