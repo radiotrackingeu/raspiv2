c5cae12a5a19a12d6d0e2197341f3e9a24225419
ralf <ralf.zeidler@fridata.de>
2017-08-25 19:07:44 +0200

    escpae string

--- html/sdr/cronjob_logger.sh
+++ html/sdr/cronjob_logger.sh
@@ -5,5 +5,5 @@ NEWSTRING="$2"
 
 FILE="/tmp/crontab"
 grep -q "$OLDSTRING" $FILE && 
-	sed -i "/.*$OLDSTRING*./c\\$NEWSTRING" $FILE || echo "$NEWSTRING \n#" >> $FILE
+	sed -i "/.*\$OLDSTRING*./c\\$NEWSTRING" $FILE || echo "$NEWSTRING \n#" >> $FILE
 	
