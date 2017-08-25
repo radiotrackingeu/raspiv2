36a3eb90e1b51c98b3aab7c322df2bec7e4ac1da
ralf <ralf.zeidler@fridata.de>
2017-08-25 18:43:43 +0200

    test

--- html/sdr/rtl_fftw.php
+++ html/sdr/rtl_fftw.php
@@ -342,6 +342,22 @@
 			echo '</pre>';
 			echo "System will now start logger upon start with the following settings: Frequency: ".$_POST["center_freq"]." Frequency-Range: ".$_POST["freq_range"]." Log-Level: ".$_POST["log_level"]." Gain: " . $_POST["log_gain"]. " and File-Name: ". $_POST["log_name"];
 		}
+		if($_POST["start_timer"]="start_no"){
+			$change= "#@reboot root " .$cmd;
+			$search = "sudo docker run --rm -t --device=/dev/bus/usb -v /var/www/html/sdr/record/:/home/ rtl_433_mod bash";
+			echo '<pre>';
+			$result = system("sudo docker run -t --rm --privileged --net=host -v /var/www/html/sdr/:/tmp1/ -v /etc/:/tmp/ git sh /tmp1/cronjob_logger.sh \"".$search."\" \"".$change."\"", $ret);
+			echo '</pre>';
+			echo "System will now start logger upon start with the following settings: Frequency: ".$_POST["center_freq"]." Frequency-Range: ".$_POST["freq_range"]." Log-Level: ".$_POST["log_level"]." Gain: " . $_POST["log_gain"]. " and File-Name: ". $_POST["log_name"];
+		}
+		if($_POST["start_timer"]="start_on_time"){
+			$change= $_POST["start_min"]. " ".$_POST["start_hour"]." root " .$cmd;
+			$search = "sudo docker run --rm -t --device=/dev/bus/usb -v /var/www/html/sdr/record/:/home/ rtl_433_mod bash";
+			echo '<pre>';
+			$result = system("sudo docker run -t --rm --privileged --net=host -v /var/www/html/sdr/:/tmp1/ -v /etc/:/tmp/ git sh /tmp1/cronjob_logger.sh \"".$search."\" \"".$change."\"", $ret);
+			echo '</pre>';
+			echo "System will now start logger upon start with the following settings: Frequency: ".$_POST["center_freq"]." Frequency-Range: ".$_POST["freq_range"]." Log-Level: ".$_POST["log_level"]." Gain: " . $_POST["log_gain"]. " and File-Name: ". $_POST["log_name"];
+		}
 	}
 	function unliveExecuteCommand($cmd)
 	{
