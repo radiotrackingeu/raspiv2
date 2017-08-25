610608a6a718eb56abfef6aacbe8e81cf32c7ffe
ralf <ralf.zeidler@fridata.de>
2017-08-25 15:29:54 +0200

    sh script mssing

---
+++ html/sdr/cronjob_logger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+
+OLDSTRING="$1"
+NEWSTRING="$2"
+FILE="/tmp/crontab"
+grep -q "$OLDSTRING" $FILE && 
+	sed -i "/$OLDSTRING/c\\$NEWSTRING" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
+	
