7c4ed18f6ffaafc108814c481654ffead8f33e65
ralf <ralf.zeidler@fridata.de>
2017-08-29 18:28:33 +0200

    new try replacing

--- html/sdr/cronjob_logger.sh
+++ html/sdr/cronjob_logger.sh
@@ -5,7 +5,7 @@ NEWSTRING=$2
 
 FILE="/tmp/crontab"
 grep -q "$OLDSTRING" $FILE && 
-	sed -i "/.*\$OLDSTRING.*/c\\$NEWSTRING" $FILE || echo "$NEWSTRING \n#" >> $FILE
+	sed -i "|$OLDSTRING| c\$NEWSTRING" $FILE || echo "$NEWSTRING \n#" >> $FILE
 	
 echo $OLDSTRING
 echo $NEWSTRING
