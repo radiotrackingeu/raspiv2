1e97e85a1b7029a08c29cd33f88aaf2409cf3836
ralf <ralf.zeidler@fridata.de>
2017-09-04 11:24:00 +0200

    --rebase

--- Docker/gitlab/update_html.sh
+++ Docker/gitlab/update_html.sh
@@ -12,7 +12,7 @@
 # Running on live branch with githab repository
 cd /home/pi/gitrep/raspiv2/
 echo 'Download new repositry:'
-git pull
+git pull --rebase
 git checkout $1
 echo Switched to $1 branch.
 echo '<br> Copy new repositry.'
