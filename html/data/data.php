7d07dc14a008b04a243db7fa6ff348240fc0f9bc
ralf <ralf.zeidler@fridata.de>
2017-09-05 17:46:08 +0200

    added data management for logger data

--- html/data/data.php
+++ html/data/data.php
@@ -18,46 +18,38 @@
 <!-- Enter text here-->
 
 <div class="w3-bar w3-brown w3-mobile">
-  <button class="w3-bar-item w3-button w3-mobile" onclick="openCity('camera1')">Camera</button>
-  <button class="w3-bar-item w3-button w3-mobile" onclick="openCity('radio1')">Radio</button>
+	<button class="w3-bar-item w3-button w3-mobile" onclick="openCity('camera_data')">Camera</button>
+	<button class="w3-bar-item w3-button w3-mobile" onclick="openCity('radio_data')">Radio</button>
 </div>
 
-<div id="camera1" class="w3-container city" style="display:none">
+<div id="camera_data" class="w3-container city" style="display:none">
 	<div class="w3-panel w3-green w3-round">
 		<br>
 
 		<h3>Zip Camera's record folder</h3><br>
 		<form method='POST' enctype="multipart/form-data">
-			<input type="text" name="zip_name" value="<?php echo date('Y_m_d_H_i')?>">
+			<input type="text" name="zip_name" value="<?php echo "Camera_".date('Y_m_d_H_i')?>">
 			<input type="submit" class="w3-btn w3-brown" value="Zip All Camera Recordings" name="zip_camera" /> <br><br>
 			You can find the zipped files here: <a href="/picam/zipped/">Record Folder</a> <br><br>
-			<input type="submit" class="w3-btn w3-brown" value="Delete all recordings" name="rm_record_folder" />
-			<input type="submit" class="w3-btn w3-brown" value="Delete all zipped files" name="rm_zip_folder" /><br><br>
+			<input type="submit" class="w3-btn w3-brown" value="Delete all recordings" name="rm_cam_record_folder" />
+			<input type="submit" class="w3-btn w3-brown" value="Delete all zipped files" name="rm_cam_zip_folder" /><br><br>
 
 		</form>
 		<br>
 	</div>
 </div>
-<div id="radio1" class="w3-container city" style="display:none">
+<div id="radio_data" class="w3-container city" style="display:none">
 	<div class="w3-panel w3-green w3-round">
 		<br>
-		To record a Frequency Spektrum for a given time, just modify the entries below and press Start.
-		<h3>Record properties</h3><br>
+
+		<h3>Zip Logger's record folder</h3><br>
 		<form method='POST' enctype="multipart/form-data">
-			<table style="width:90%">
-				<tr>
-					<td>Gain in DB:</td>
-					<td><input type="text" name="log_gain" value="20"></td>
-					<td>Gain of the recording device. Higher gain results in more noise.</td>
-				</tr>
-				<tr>
-					<td>Zip Name:</td>
-					<td><input type="text" name="zipasd_name" value="<?php echo date('Y_m_d_H_i')?>"></td>
-					<td>You can find the results here: <a href="/picam/zipped/">Record Folder</a></td>
-				</tr>
-			</table>
-			<input type="submit" class="w3-btn w3-brown" value="Start" name="log_start" />
-			<input type="submit" class="w3-btn w3-brown" value="Stop" name="log_stop" />
+			<input type="text" name="zip_name" value="<?php echo "Logger_".date('Y_m_d_H_i')?>">
+			<input type="submit" class="w3-btn w3-brown" value="Zip All Logger Recordings" name="zip_logger" /> <br><br>
+			You can find the zipped files here: <a href="/sdr/zipped/">Record Folder</a> <br><br>
+			<input type="submit" class="w3-btn w3-brown" value="Delete all recordings" name="rm_logger_record_folder" />
+			<input type="submit" class="w3-btn w3-brown" value="Delete all zipped files" name="rm_logger_zip_folder" /><br><br>
+
 		</form>
 		<br>
 	</div>
@@ -76,15 +68,29 @@
 				$test = system("sudo docker run -t --rm --privileged -v /var/www/html/picam/:/tmp/ git zip -r /tmp/zipped/".$_POST["zip_name"]." /tmp/record/ 2>&1", $ret);
 				echo '</pre>';
 			}
-			
-			if (isset($_POST["rm_zip_folder"])){
+			if (isset($_POST["rm_cam_zip_folder"])){
 				echo '<pre>';
 				$test = system("rm -rf /var/www/html/picam/zipped/* 2>&1", $ret);
 				echo '</pre>';
 			}
-			if (isset($_POST["rm_record_folder"])){
+			if (isset($_POST["rm_cam_record_folder"])){
 				echo '<pre>';
 				$test = system("rm -rf /var/www/html/picam/record/* 2>&1", $ret);
+				echo '</pre>';
+			}
+			if (isset($_POST["zip_logger"])){
+				echo '<pre>';
+				$test = system("sudo docker run -t --rm --privileged -v /var/www/html/sdr/:/tmp/ git zip -r /tmp/zipped/".$_POST["zip_name"]." /tmp/record/ 2>&1", $ret);
+				echo '</pre>';
+			}
+			if (isset($_POST["rm_logger_zip_folder"])){
+				echo '<pre>';
+				$test = system("rm -rf /var/www/html/sdr/zipped/* 2>&1", $ret);
+				echo '</pre>';
+			}
+			if (isset($_POST["rm_logger_record_folder"])){
+				echo '<pre>';
+				$test = system("rm -rf /var/www/html/sdr/record/* 2>&1", $ret);
 				echo '</pre>';
 			}
 
