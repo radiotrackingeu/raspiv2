aeb5e94b17e18cd458d7996a423fc61886094348
ralf <ralf.zeidler@fridata.de>
2017-09-08 15:27:02 +0200

    two outputs?

--- html/sdr/rtl_fm.php
+++ html/sdr/rtl_fm.php
@@ -54,11 +54,11 @@
 			<input type="submit" class="w3-btn w3-brown" value="Start Local playback" name="rtl_fm_start_l"/>
 			<input type="submit" class="w3-btn w3-brown" value="Stop" name="rtl_stop"/>
 			<br>
-			<br>
 			<audio controls>
 				<source src="http://<?php echo $_SERVER['SERVER_NAME'].":".($_SERVER['SERVER_PORT']+1); ?>" type="audio/mpeg" controls preload="none">
 				Your browser does not support the audio element.
 			</audio>
+			<br><br>
 		</form>
 	</div>
 </div>
@@ -72,9 +72,14 @@
 			<input type="submit" class="w3-btn w3-brown" value="<?php echo $config['SDR_Radio']['Freq3'] ?>" name="rtl_fm_start_f3"/>
 			<input type="submit" class="w3-btn w3-brown" value="<?php echo $config['SDR_Radio']['Freq4'] ?>" name="rtl_fm_start_f4"/><br><br>
 			<input type="submit" class="w3-btn w3-brown" value="<?php echo $config['SDR_Radio']['Freq5'] ?>" name="rtl_fm_start_f5"/>
-			<input type="submit" class="w3-btn w3-brown" value="<?php echo $config['SDR_Radio']['Freq6'] ?>" name="rtl_fm_start_f6"/><br><br>
+			<input type="submit" class="w3-btn w3-brown" value="<?php echo $config['SDR_Radio']['Freq6'] ?>" name="rtl_fm_start_f6"/><br><br><br>
+			<input type="submit" class="w3-btn w3-brown" value="Stop" name="rtl_stop"/>
 		</form>
 		<br><br>
+		<audio controls>
+			<source src="http://<?php echo $_SERVER['SERVER_NAME'].":".($_SERVER['SERVER_PORT']+1); ?>" type="audio/mpeg" controls preload="none">
+			Your browser does not support the audio element.
+		</audio>
 	</div>
 </div>
 
