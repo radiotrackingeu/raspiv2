2f2949b34a3fd4107f7ad8b5ce16b6f8f24d89da
ralf <ralf.zeidler@fridata.de>
2017-09-13 15:46:18 +0200

    test 4000

--- html/sdr/liquidsdr/rtlsdr_signal_detect.c
+++ html/sdr/liquidsdr/rtlsdr_signal_detect.c
@@ -13,14 +13,14 @@
 #include <liquid/liquid.h>
 #include <getopt.h>
 
-#define nfft (400)
+#define nfft (4000)
 
 float psd_template[nfft];
 float psd         [nfft];
 int   detect      [nfft];
 int   count       [nfft];
 int   groups      [nfft];
-int   timestep    =nfft/32; // time between transforms [samples]
+int   timestep    =nfft/16; // time between transforms [samples]
 unsigned long int num_transforms = 0;
 
 // print usage/help message
