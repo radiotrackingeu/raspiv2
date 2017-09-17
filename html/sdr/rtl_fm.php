bf40ef6d45b913fc7d07562871f6584c9f498f69
ralf <ralf.zeidler@fridata.de>
2017-09-18 00:09:17 +0200

    good night

--- html/sdr/rtl_fm.php
+++ html/sdr/rtl_fm.php
@@ -33,7 +33,9 @@
 </div>
 <div id="single_freq" class="w3-container city" style="display:none">
 	<div class="w3-panel w3-green w3-round">
+	
 		<form method="post" enctype="multipart/form-data" action="<?php update_Config($config); echo $_SERVER['PHP_SELF'];?>">
+			<br>
 			<select name="device">
 				<option value=1 >Receiver 1</option>
 				<option value=2 >Receiver 2</option>
