71e43339c0d3094442962b00d5ca49aa3c24def5
ralf <ralf.zeidler@fridata.de>
2017-09-17 23:20:41 +0200

    now?

--- html/sdr/rtl_fm.php
+++ html/sdr/rtl_fm.php
@@ -39,20 +39,21 @@
 				if($config['SDR_Radio']['device']==1){
 					echo "<strong>Receiver 1 selected</strong><br><br>";
 					echo "<input type='submit' class='w3-btn w3-brown' value='Switch to receiver 2' name='change_device_to_2'/>";
-					if (isset($_POST["change_device_to_2"])){
-						$config['SDR_Radio']['device']==2;
-						update_Config($config);
-						echo "<script type='text/javascript'>document.getElementById('single_freq').style.display = 'block';</script>";
-					}
+
+				}
+				if (isset($_POST["change_device_to_2"])){
+					$config['SDR_Radio']['device']==2;
+					update_Config($config);
+					echo "<script type='text/javascript'>document.getElementById('single_freq').style.display = 'block';</script>";
 				}
 				if($config['SDR_Radio']['device']==2){
 					echo "<strong>Receiver 2 selected</strong><br><br>";
 					echo "<input type='submit' class='w3-btn w3-brown' value='Switch to receiver 1' name='change_device_to_1'/>";
-					if (isset($_POST["change_device_to_1"])){
-						$config['SDR_Radio']['device']==1;
-						update_Config($config);
-						echo "<script type='text/javascript'>document.getElementById('single_freq').style.display = 'block';</script>";
-					}
+				}
+				if (isset($_POST["change_device_to_1"])){
+					$config['SDR_Radio']['device']==1;
+					update_Config($config);
+					echo "<script type='text/javascript'>document.getElementById('single_freq').style.display = 'block';</script>";
 				}
 			?>
 			<br><br>
