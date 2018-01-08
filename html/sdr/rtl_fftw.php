982cc0d93d0b51fdc512dc82343bc0d619f82c1d
radiotrackingeu <software@radio-tracking.eu>
2018-01-08 17:25:01 +0100

    oops again

--- html/sdr/rtl_fftw.php
+++ html/sdr/rtl_fftw.php
@@ -212,7 +212,7 @@
 	</div>
 </div>
 -->
-<!--
+<!---->
 <div id="tab_raw_data_ana" class="w3-container city" style="display:none">
 	<div class="w3-panel w3-green w3-round">
 		<form method="POST" enctype="multipart/form-data" action="<?php update_Config($config); echo $_SERVER['PHP_SELF']; ?>" >
