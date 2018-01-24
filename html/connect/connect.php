6d1602bbba7debb71136e6ad5d41aa22ba10cd1b
ralf <ralf.zeidler@fridata.de>
2018-01-24 21:14:34 +0100

    new connection page

--- html/connect/connect.php
+++ html/connect/connect.php
@@ -28,23 +28,8 @@
 
 
 <div class="w3-bar w3-brown">
-  <button class="w3-bar-item w3-button w3-mobile" onclick="openCity('Both')">Connect</button>
-  <button class="w3-bar-item w3-button w3-mobile" onclick="openCity('UMTS')">Mobile</button>
   <button class="w3-bar-item w3-button w3-mobile" onclick="openCity('VPN')">Tunnel</button>
   <button class="w3-bar-item w3-button w3-mobile" onclick="openCity('VPN_Setup')">Tunnel Setup</button>
-  <button class="w3-bar-item w3-button w3-mobile" onclick="openCity('UMTS_Setup')">Mobile Setup</button>
-</div>
-
-
-<div id="UMTS" class="w3-container city" style="display:none">
-<div class="w3-panel w3-green w3-round-xlarge">
-	<form method="post" enctype="multipart/form-data" action="<?php update_Config($config); echo $_SERVER['PHP_SELF']; ?>">
-		<p>UMTS is used when no other Internet Connection is avaible. The proper stick needs to be pluged in and installed.</p>
-		<input type="submit" class="w3-btn w3-brown" value="Start UMTS" name="start_umts">
-		<input type="submit" class="w3-btn w3-brown" value="Stop UMTS" name="stop_umts">
-		<br><br>
-	</form>
-</div>
 </div>
 
 <div id="VPN" class="w3-container city" style="display:none">
@@ -64,36 +49,6 @@
 	</div>
 </div>
 
-<div id="Both" class="w3-container city" style="display:none">
-	<div class="w3-panel w3-green w3-round-xlarge">
-		<form method="post" enctype="multipart/form-data" action="<?php update_Config($config); echo $_SERVER['PHP_SELF']; ?>">
-			<p>First UMTS will be connected then the VPN</p>
-			<input type="submit" class="w3-btn w3-brown" value="Start Both" name="start_umts_vpn">
-			<input type="submit" class="w3-btn w3-brown" value="Stop Both" name="stop_umts_vpn">
-			<br><br>
-		</form>
-	</div>
-</div>
-
-<div id="UMTS_Setup" class="w3-container city" style="display:none">
-	<div class="w3-panel w3-green w3-round-xlarge">
-		<form method="post" enctype="multipart/form-data" action="<?php update_Config($config); echo $_SERVER['PHP_SELF']; ?>">
-			<p>APN needs to be changed according to the provider's needs. The Swtich Mode must be executed if the UMTS Dongle has been removed while the system was running. </p>
-			<input type="submit" class="w3-btn" value="Swtich Mode" name="switch_mode">
-
-			Standard Settings: Edeka Mobil
-			<br><br>
-			<label class="w3-label">First Name</label>
-			<input type="text" class="w3-input" value="&quot;data.access.de&quot;" name="apn">
-			
-			<label class="w3-label">First Name</label>
-			<input type="text" class="w3-input" value="*99***1#" name="dial">
-			<input type="submit" class="w3-btn" value="Change Settings" name="change_wvdial">
-			<br><br>
-		</form>
-	</div>
-</div>
-
 <div id="VPN_Setup" class="w3-container city" style="display:none">
 	<div class="w3-panel w3-green w3-round-xlarge">
 		<form method="post" enctype="multipart/form-data" action="<?php update_Config($config); echo $_SERVER['PHP_SELF']; ?>">
@@ -101,7 +56,7 @@
 			<input type="file" name="fileToUpload" id="fileToUpload">
 			<input type="submit" value="Upload Certificate" name="upload_cfg">
 			<br><br><br>
-			<input type="submit" class="w3-btn" value="Remove Certificate" name="rm_config">
+			<input type="submit" class="w3-btn w3-brown" value="Remove Certificate" name="rm_config">
 			<br><br>
 		</form>
 	</div>
