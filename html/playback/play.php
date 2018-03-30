972d1211f213e7742da4892bcdcba416f633fcd0
ralf <ralf.zeidler@fridata.de>
2018-03-30 16:03:45 +0200

    echo total

--- html/playback/play.php
+++ html/playback/play.php
@@ -98,23 +98,24 @@
 		}
 		
 		if (isset($_POST["ul_wav"])) {
-			$total = count($_FILES["file_wav"]["tmp_name"]);
+			$total = count($_FILES["file_wav"]["name"]);
+			echo $total;
 			if ($total==0)
 				echo "No files given!<br>";
 			else
 				for ($i=0; $i<$total; $i++) {
-					if ($_FILES["file_wav"]["tmp_name"] == "") continue;
+					if ($_FILES["file_wav"]["name"] == "") continue;
 					if ($_FILES["file_wav"]["size"] == 0 ) continue;
-					$finfo = finfo_open(FILEINFO_MIME_TYPE);
-					$mtype = finfo_file($finfo,$_FILES["file_wav"]["tmp_name"][$i]);
-					if ($mtype != "audio/x-wav") {
-						echo $_FILES["file_wav"]["tmp_name"][$i]." is not a .wav file and was skipped.<br>";
-						continue;
-					}
-					if (move_uploaded_file($_FILES["file_wav"]["tmp_name"][$i], "files/".$_FILES["file_wav"]["tmp_name"][$i])){
-								echo "Successfully uploaded ".$_FILES["file_wav"]["tmp_name"][$i].".<br>";
+					#$finfo = finfo_open(FILEINFO_MIME_TYPE);
+					#$mtype = finfo_file($finfo,$_FILES["file_wav"]["tmp_name"][$i]);
+					#if ($mtype != "audio/x-wav") {
+					#	echo $_FILES["file_wav"]["name"][$i]." is not a .wav file and was skipped.<br>";
+					#	continue;
+					#}
+					if (move_uploaded_file($_FILES["file_wav"]["tmp_name"][$i], "files/".$_FILES["file_wav"]["name"][$i])){
+								echo "Successfully uploaded ".$_FILES["file_wav"]["name"][$i].".<br>";
 							} else {
-								echo "Could not upload ".$_FILES["file_wav"]["tmp_name"][$i]."!<br>";
+								echo "Could not upload ".$_FILES["file_wav"]["name"][$i]."!<br>";
 							}
 				}
 		}
