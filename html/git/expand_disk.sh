93b6ef4cbc69648499e8b2ecc2bd5eedf7203971
Philipp Mallot <philipp.mallot@fridata.de>
2018-06-26 17:55:47 +0200

    expand disk typo

--- html/git/expand_disk.sh
+++ html/git/expand_disk.sh
@@ -3,5 +3,5 @@
 #expand system partition on reboot
 
 FILE="/tmp/crontab"
-printf "@reboot root /usr/bin/raspi-config --expand-rootfs\n" >> $FILE
+printf "\n@reboot root /usr/bin/raspi-config --expand-rootfs\n" >> $FILE
 printf "@reboot root sed -i '\|^@reboot.*raspi-config --expand-rootfs.*$|d' /etc/crontab\n#" >> $FILE
