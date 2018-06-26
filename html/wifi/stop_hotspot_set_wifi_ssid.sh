ae38959f7fffd831229f3cb896c3b308a8974420
Philipp Mallot <philipp.mallot@fridata.de>
2018-06-26 15:23:16 +0200

    WiFi: revert to last working version

--- html/wifi/stop_hotspot_set_wifi_ssid.sh
+++ html/wifi/stop_hotspot_set_wifi_ssid.sh
@@ -2,6 +2,12 @@
 # $1: SSID
 # $2: PW
 
+# change /etc/crontab and comment docker wifi line
+FILE="/tmp/crontab"
+OLDSTRING=$(grep -P 'wifi:\d\.\d' $FILE)
+NEWSTRING="#$OLDSTRING"
+grep -q "$OLDSTRING" $FILE && 
+    sed -i "s~^$OLDSTRING~$NEWSTRING~g" $FILE || echo -e "$NEWSTRING \n#" >> $FILE
 # change ssid and password
 
 OLDSTRING="^ssid.*"
