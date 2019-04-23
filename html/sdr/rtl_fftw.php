473e96167674e603ce5eecdb4e4bbbbaaa247483
Philipp Mallot <philipp.mallot@fridata.de>
2019-04-23 14:26:24 +0200

    fix tooltips in logger settings Closes #112

--- html/sdr/rtl_fftw.php
+++ html/sdr/rtl_fftw.php
@@ -175,10 +175,10 @@
                     <div class="w3-panel w3-green w3-round">
                         <h3>Receiver <?=$i?>
                             <?php if ($i==0) :?> 
-                                <span class="w3-tooltip" style="float:right">
-                                    <span class="w3-text w3-small w3-round w3-brown w3-text" style="position:absolute; right:0; top:25px; width:250px; padding: 3px">Copy settings (except name and orientation) to all receivers.</span>
-                                    <i class="fas fa-clone" onclick="copyInput('rec_settings', ['antenna_id_', 'antenna_orientation_', 'change_logger_setting'])" style="cursor:pointer;float:right"></i>
-                                </span>
+                                <div style="display:inline-block; float:right">
+                                    <i class="fas fa-clone rteutooltip" onclick="copyInput('rec_settings', ['antenna_id_', 'antenna_orientation_', 'change_logger_setting'])" style="cursor:pointer"></i>
+                                    <span class="rteutooltiptext" >Copy settings (except name and orientation) to all receivers.</span>
+                                </div>
                             <?php endif;?>
                         </h3><br>
                         <button type=button onclick="myAccordion('rec<?=$i?>_settings')" class="w3-button w3-green w3-block w3-left-align"><h4>Receiver Settings</h4></button>
@@ -209,10 +209,10 @@
                                 <small>This - together with a timestamp - will be used as filename and antenna id in the database.</small>
                             </p>
                             Antenna Position:&emsp;
-                            <span class="w3-tooltip">
-                                <i class="fas fa-map-marker-alt" onclick="getLocation(<?=$i?>,1)" style="cursor:pointer"></i>
-                                <span class="w3-text w3-small w3-round w3-brown w3-tag" style="position:absolute; left:0; top:25px; width:250px; padding: 3px">Get Position from Device.</span>
-                            </span><br>
+                            <div style="display:inline-block">
+                                <i class="fas fa-map-marker-alt rteutooltip" onclick="getLocation(<?=$i?>,1)" style="cursor:pointer"></i>
+                                <span class="rteutooltiptext" >Get Position from Device.</span>
+                            </div><br>
                             <div id="input_position_<?=$i?>" class="w3-half" style = "margin-bottom: 16px">
                                 <label>Latitude</label>
                                 <input class="w3-input w3-mobile" style="width:30%" type="text" id="antenna_position_N_<?=$i?>" name="antenna_position_N_<?=$i?>" value="<?php echo isset($config['logger']['antenna_position_N_'.$i]) ? $config['logger']['antenna_position_N_'.$i] : 1.234?>">
@@ -223,15 +223,15 @@
                             </div>
                             <p>
                                 Antenna Orientation in degrees (i.e. N=0, E=90, S=180):&emsp;
-                                <span class="w3-tooltip">
-                                    <i class="fas fa-location-arrow" onclick="getLocation(<?=$i?>,0)" style="cursor:pointer"></i>
-                                    <span class="w3-text w3-small w3-round w3-brown w3-tag" style="position:absolute; right:0; top:25px; width:250px; padding: 3px">Get Orientation from Device.</span>
-                                </span>
+                                <div style="display:inline-block">
+                                    <i class="fas fa-location-arrow rteutooltip" onclick="getLocation(<?=$i?>,0)" style="cursor:pointer"></i>
+                                    <span class="rteutooltiptext" >Get Orientation from Device.</span>
+                                </div>
                                 <?php if ($i==0) :?> 
-                                  &emsp; <span class="w3-tooltip">
-                                      <span class="w3-text w3-small w3-round w3-brown w3-text" style="position:absolute; right:0; top:25px; width:250px; padding: 3px">Set other antenna orientations accordingly (i.e. value below +90 / +180 / +270).</span>
-                                      <i class="fas fa-forward" onclick="propagateAngle()" style="cursor:pointer"></i>
-                                  </span>
+                                  &emsp; <div style="display:inline-block">
+                                      <i class="fas fa-forward rteutooltip" onclick="propagateAngle()" style="cursor:pointer"></i>
+                                      <span class="rteutooltiptext">Set other antenna orientations accordingly (i.e. value below +90 / +180 / +270).</span>
+                                  </div>
                                 <?php endif;?><br><br>
                                 <input class="w3-input w3-mobile" style="width:30%" type="text" id="antenna_orientation_<?=$i?>" name="antenna_orientation_<?=$i?>" value="<?php echo isset($config['logger']['antenna_orientation_'.$i]) ? $config['logger']['antenna_orientation_'.$i] : 42?>">
                             </p>
